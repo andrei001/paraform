f5fcd7d41ff71ecd13860c3f410cd319a648afa2
Paul Chicos <paulchicos@Pauls-MBP.lan>
2024-12-13 19:40:14 -0500

    add material ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,215 @@
-import Image from "next/image";
+"use client";
+
+import {
+  Box,
+  Button,
+  Input,
+  InputLabel,
+  MenuItem,
+  Select,
+} from "@mui/material";
+import { useEffect, useState } from "react";
+
+const form = [
+  {
+    type: "text",
+    placeholder: "Jane",
+    id: "firstName",
+    label: "First Name",
+    required: true,
+  },
+  {
+    type: "text",
+    placeholder: "Doe",
+    id: "lastName",
+    label: "Last Name",
+    required: true,
+  },
+  {
+    type: "text",
+    placeholder: "Apple",
+    id: "company",
+    label: "Company",
+    required: false,
+  },
+  {
+    type: "text",
+    placeholder: "Software Engineer",
+    id: "title",
+    label: "Title",
+    required: false,
+  },
+  {
+    type: "phoneNumbers",
+    id: "phoneNumbers",
+    title: "Phone Numbers",
+    options: ["home", "work", "mobile", "skype", "other"],
+    required: false,
+  },
+  {
+    type: "addresses",
+    id: "addresses",
+    title: "Addresses",
+    required: false,
+    options: ["home", "work", "other"],
+  },
+  {
+    type: "email_addresses",
+    id: "email_addresses",
+    title: "Email Addresses",
+    options: ["personal", "work", "other"],
+    required: false,
+  },
+  {
+    type: "website_addresses",
+    id: "website_addresses",
+    title: "Website Addresses",
+    options: ["personal", "company", "portfolio", "blog", "other"],
+    required: false,
+  },
+  {
+    type: "social_media_addresses",
+    id: "social_media_addresses",
+    title: "Social Media Addresses",
+    required: false,
+  },
+  { type: "educations", id: "educations", required: false },
+  { type: "employments", id: "employments", required: false },
+];
+
+const DynamicForm = ({
+  title,
+  id,
+  options,
+  required,
+}: {
+  title: string;
+  id: string;
+  options: string[];
+  required: boolean;
+}) => {
+  const [textInput, setTextInput] = useState<string[]>([""]);
+  const [selectedOption, setSelectedOption] = useState<string[] | undefined>(
+    options.length !== 0 ? [options[0]] : undefined,
+  );
+  return (
+    <Box display="flex" flexDirection="column" marginTop="16px">
+      <InputLabel>{title}</InputLabel>
+      <Box>
+        {textInput.map((e, index) => (
+          <div style={{display:"flex", flexDirection:"row", marginBottom: "8px"}}>
+            <Input
+              type="text"
+              fullWidth
+              id={id}
+              onChange={(e) =>
+                setTextInput([
+                  ...textInput.slice(0, index),
+                  e.target.value,
+                  ...textInput.slice(index + 1),
+                ])
+              }
+              required={required}
+            />
+            {options.length !== 0 && selectedOption !== undefined && (
+              <Select
+                value={selectedOption[index]}
+                onChange={(e) =>
+                  setSelectedOption([
+                    ...selectedOption.slice(0, index),
+                    e.target.value,
+                    ...selectedOption.slice(index + 1),
+                  ])
+                }
+                label={title}
+                variant="standard"
+              >
+                {options.map((opt: string, index: number) => (
+                  <MenuItem key={index} value={opt}>
+                    {opt}
+                  </MenuItem>
+                ))}
+              </Select>
+            )}
+          </div>
+        ))}
+      </Box>
+      <Button
+          onClick={() => {
+            setTextInput([...textInput, ""]);
+            if (selectedOption !== undefined)
+              setSelectedOption([...selectedOption, options[0]]);
+          }}
+        >
+          Add Another
+        </Button>
+    </Box>
+  );
+};
+
+const SimpleText = ({
+  id,
+  label,
+  required,
+}: {
+  id: string;
+  label: string;
+  required: boolean;
+}) => {
+  const [textInput, setTextInput] = useState<string>("");
+  return (
+    <Box display="flex" flexDirection="column">
+      <InputLabel htmlFor={id}>{label}</InputLabel>
+      <Input
+        required={required}
+        type="text"
+        id={id}
+        onChange={(e) => setTextInput(e.target.value)}
+      />
+    </Box>
+  );
+};
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [schoolsList, setSchoolsList] = useState<string | undefined>(undefined);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('https://harvest.greenhouse.io/v1/schools', {
+        method: 'GET',
+        mode: 'no-cors',
+        headers: {
+          'Authorization': 'Basic ZjA2YjJiMTUzZTAxNmY4ZTdjMzYzMjYyN2FmNTZiMWQtNzo=',
+          'Content-Type': 'application/json'
+        }
+      });
+      const result = await response.json();
+      console.log(result)
+      setSchoolsList(result);
+    };
+
+    fetchData();
+  }, []);
+  return (
+    <Box
+      display="flex"
+      alignContent="center"
+      flexDirection="column"
+      margin="auto"
+      width="80vh"
+    >
+      {form.map((e) =>
+        e.type === "text" ? (
+          <SimpleText id={e.id} label={e.label ?? ""} required={e.required} />
+        ) : (
+          <DynamicForm
+            title={e.title ?? ""}
+            id={e.id}
+            options={e.options ?? []}
+            required={e.required}
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        ),
+      )}
+    </Box>
   );
 }
